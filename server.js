e97cf2a037b2f4afa9325a9f2d5ce4e8c4cc644c
mrbirring <159231749+mrbirring@users.noreply.github.com>
2025-05-20 17:46:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-// server.js
 const io = require('socket.io')(3000, {
-    cors: { origin: '*' }
+  cors: { origin: '*' }
+});
+
+let rooms = {};
+
+io.on('connection', socket => {
+  console.log('Connected:', socket.id);
+
+  socket.on('createRoom', roomName => {
+    if (rooms[roomName]) {
+      socket.emit('roomError', 'Room already exists');
+      return;
+    }
+    rooms[roomName] = [socket.id];
+    socket.join(roomName);
+    socket.emit('roomCreated', roomName);
   });
-  
-  io.on('connection', socket => {
-    console.log('A user connected:', socket.id);
-  
-    socket.on('join', userId => {
-      console.log(`User ${userId} joined`);
-      socket.join(userId);
-    });
-  
-    socket.on('message', ({ to, from, message }) => {
-      console.log(`Message from ${from} to ${to}: ${message}`);
-      io.to(to).emit('message', { from, message });
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
-    });
+
+  socket.on('joinRoom', roomName => {
+    if (!rooms[roomName]) {
+      socket.emit('roomError', 'Room does not exist');
+      return;
+    }
+    rooms[roomName].push(socket.id);
+    socket.join(roomName);
+    socket.emit('roomJoined', roomName);
+    io.to(roomName).emit('message', { from: 'System', message: `${socket.id} joined` });
   });
-  
+
+  socket.on('message', ({ roomName, from, message }) => {
+    io.to(roomName).emit('message', { from, message });
+  });
+
+  socket.on('disconnect', () => {
+    for (const room in rooms) {
+      rooms[room] = rooms[room].filter(id => id !== socket.id);
+      if (rooms[room].length === 0) delete rooms[room];
+    }
+    console.log('Disconnected:', socket.id);
+  });
+});
